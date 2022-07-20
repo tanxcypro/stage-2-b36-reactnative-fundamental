ac0b7566d02cfe6a329b7015ba425af5b75759dc
Your Name <samsulr99@gmail.com>
2022-07-20 10:46:35 +0700

    update

--- src/screens/flatList.js
+++ src/screens/flatList.js
@@ -1,13 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { FlatList, View, Text } from "react-native";
 
 export default function List() {
   // Make Dummy Data with Array
+  const socialMedia = [
+    {
+      title: 'Instagram',
+    },
+    {
+      title: 'Facebook',
+    },
+    {
+      title: 'Twitter',
+    },
+    {
+      title: 'Tiktok',
+    },
+  ];
+
+  const renderItem = ({ item }) => (
+    <Text>{item.title}</Text>
+  );
+
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+      <FlatList
+        data={socialMedia}
+        keyExtractor={item => item.title}
+        renderItem={renderItem}
+      />
+
     </View>
   );
 }
