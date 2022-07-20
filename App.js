fae73acabe6dfc342ac2eef73d169be161d7310a
Your Name <samsulr99@gmail.com>
2022-07-20 10:26:38 +0700

    update

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
+import Form from "./src/screens/form";
 
 export default function App() {
   return (
     <View>
       <StatusBar />
       {/* Use Component */}
+      <Form />
     </View>
   );
 }
--- src/screens/form.js
+++ src/screens/form.js
@@ -1,14 +1,67 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
 
 export default function Form() {
   return (
-    <View>
+    <View style={style.container}>
       <StatusBar />
       {/* Code Here */}
+
+      <Text style={style.header}>SignIn</Text>
+
+      <Text style={style.labelText}>Email</Text>
+      <TextInput style={style.textInput} placeholder="email" />
+
+      <Text style={style.labelText}>Password</Text>
+      <TextInput style={style.textInput} secureTextEntry={true} placeholder="password" />
+
+      <TouchableOpacity style={style.button}>
+        <Text style={style.textButton}>Sign In</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 // Create Variable for CSS
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  header: {
+    color: 'orange',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 50
+  },
+  labelText: {
+    color: 'grey',
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 5
+  },
+  textInput: {
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 12,
+    color: 'grey',
+    padding: 10,
+    borderColor: 'grey'
+  },
+  textButton: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: 'orange',
+    height: 40,
+    width: '100%',
+    borderRadius: 5,
+    justifyContent: 'center'
+  }
+})
