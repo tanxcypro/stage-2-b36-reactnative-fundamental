9646e00cc4b05d5066b3aa9f43eddf5b86e6f734
Your Name <samsulr99@gmail.com>
2022-07-20 10:32:42 +0700

    update

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
+import Map from "./src/screens/map";
 
 export default function App() {
   return (
     <View>
       <StatusBar />
       {/* Use Component */}
+      <Map />
     </View>
   );
 }
--- src/screens/map.js
+++ src/screens/map.js
@@ -1,14 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function Map() {
   // Create Dummy Data With Array
+  const socialMedia = ['Instagram', 'Facebook', 'Twitter', 'Tiktok']
 
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+
+      {socialMedia.map((item, index) => (
+        <Text key={index}>{item}</Text>
+      ))}
+
+      {/* <View>
+        {socialMedia.map((item) => {
+          return(
+            <Text>{item}</Text>
+          )
+        })}
+      </View> */}
+      
     </View>
   );
 }
