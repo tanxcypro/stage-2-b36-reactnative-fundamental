e835e59977ed57a069a534ae1b80572e33946ccb
Your Name <samsulr99@gmail.com>
2022-07-20 09:45:37 +0700

    update

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
+import EmbedExpression from "./src/screens/embedExpression";
 
 export default function App() {
   return (
     <View>
       <StatusBar />
       {/* Use Component */}
+      <EmbedExpression />
     </View>
   );
 }
--- src/screens/embedExpression.js
+++ src/screens/embedExpression.js
@@ -1,16 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function EmbedExpression() {
   // Create Function Here
 
+  function getBatch() {
+    return "Batch 36"
+  }
+
   // Create a variable Here
+  const name = "Samsul Rijal"
 
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+      <Text>Hallo {name} selamat datang di {getBatch()}</Text>
     </View>
   );
 }
